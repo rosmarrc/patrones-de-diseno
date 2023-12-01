a4512f32978a7e74a2c7e6fdbf36cfd0e8e81b2d
Rosmar Rijo Castro <79125757+rosmarrc@users.noreply.github.com>
2023-12-01 17:17:53 -0400

    Add files via upload

---
+++ ejemplo_singleton/ConexionBasedeDatos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo_singleton
+{
+    public sealed class ConexionBasedeDatos
+    {
+       public string Nombre { get; set; }
+        private ConexionBasedeDatos()
+        {
+            
+        }
+
+        private static ConexionBasedeDatos? Instancia;
+
+        public static ConexionBasedeDatos ObtenerInstancia(string nombre)
+        {
+            if (Instancia == null)
+            {
+                Instancia = new ConexionBasedeDatos();
+                Instancia.Nombre = nombre;
+
+            }
+            return Instancia;
+        }
+    }
+}
---
+++ ejemplo_singleton/ejemplo_singleton/Program.cs
@@ -0,0 +1,11 @@
+﻿using ejemplo_singleton;
+
+ConexionBasedeDatos conexion1 = ConexionBasedeDatos.ObtenerInstancia("Conexion1");
+ConexionBasedeDatos conexion2 = ConexionBasedeDatos.ObtenerInstancia("Conexion2");
+
+Console.WriteLine("-->Resultado:");
+Console.WriteLine(conexion1.Nombre);
+Console.WriteLine(conexion2.Nombre);
+Console.WriteLine("Singleton no permite nueva instancia porque ya existe una, entonces muestra la misma...");
+
+Console.ReadLine();
